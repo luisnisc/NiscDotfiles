5530de0661f67639f86211e5b71e7d032f5b9615
Nisc <luisnisc30mayo2005@gmail.com>
2026-08-20 18:31:35 +0200

    keybinds for move workspaces

--- .config/hypr/hyprland/keybinds.lua
+++ .config/hypr/hyprland/keybinds.lua
@@ -17,6 +17,9 @@ hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
 
+hl.bind("SUPER + SHIFT + LEFT", hl.dsp.workspace.move({ monitor = "l" }))
+hl.bind("SUPER + SHIFT + RIGHT", hl.dsp.workspace.move({ monitor = "r" }))
+
 for i = 1, 10 do
 	local key = i % 10
 	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
