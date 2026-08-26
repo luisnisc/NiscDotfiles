59d772ac471081f834a324be8bb7bb00888c99f2
Nisc <luisnisc30mayo2005@gmail.com>
2026-08-26 16:12:55 +0200

    obsidian window rule improve

--- .config/hypr/hyprland/rules.lua
+++ .config/hypr/hyprland/rules.lua
@@ -1,61 +1,61 @@
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
 
-    suppress_event = "maximize",
+	suppress_event = "maximize",
 })
 -- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true,
+	no_focus = true,
 })
 
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-    move  = "20 monitor_h-120",
-    float = true,
+	move = "20 monitor_h-120",
+	float = true,
 })
 
 hl.window_rule({
-    match       = { class = "org.gnome.Calendar" },
-    opacity     = "override 0.85 override 0.85 override",
-    float       = true,
-    center      = true,
-    animation   = "slide",
-    move        = { "(monitor_w * 0.5) - (window_w * 0.5)", "(monitor_h * 0.025) + 20" },
-    workspace   = "special:calendar",
+	match = { class = "org.gnome.Calendar" },
+	opacity = "override 0.85 override 0.85 override",
+	float = true,
+	center = true,
+	animation = "slide",
+	move = { "(monitor_w * 0.5) - (window_w * 0.5)", "(monitor_h * 0.025) + 20" },
+	workspace = "special:calendar",
 })
 
 hl.window_rule({
-    match       = { class = "org.pulseaudio.pavucontrol" },
-    opacity     = "override 0.85 override 0.85 override",
-    float       = true,
-    animation   = "slidevert 100%",
-    move        = { "1400", "40" },
-    workspace   = "special:pulseaudio",
+	match = { class = "org.pulseaudio.pavucontrol" },
+	opacity = "override 0.85 override 0.85 override",
+	float = true,
+	animation = "slidevert 100%",
+	move = { "1400", "40" },
+	workspace = "special:pulseaudio",
 })
 
 hl.window_rule({
-    match       = { class = "btop" },
-    opacity     = "override 0.85 override 0.85 override",
-    float       = true,
-    animation   = "slidevert 100%",
-    move        = { "1400", "40" },
-    workspace   = "special:btop",
+	match = { class = "btop" },
+	opacity = "override 0.85 override 0.85 override",
+	float = true,
+	animation = "slidevert 100%",
+	move = { "1400", "40" },
+	workspace = "special:btop",
 })
 ---
 --- GAMING
@@ -69,22 +69,19 @@ hl.window_rule({ match = { focus = false }, opacity = 0.9 })
 
 hl.window_rule({ match = { class = "waypaper" }, float = true, opacity = 1 })
 
-
 hl.window_rule({ match = { class = "blueman-manager" }, float = true, opacity = 0.8 })
-
 
 hl.window_rule({ match = { class = "nvim" }, workspace = "special:code" })
 
 hl.window_rule({ match = { class = "Spotify" }, workspace = "special:music" })
 
-hl.window_rule({ match = { class = "obsidian" }, workspace = "special:notes" })
+hl.window_rule({ match = { initial_title = "Obsidian" }, workspace = "special:notes" })
 
 hl.window_rule({ match = { class = "chrome-crunchyroll.com__-Default" }, workspace = "special:anime" })
 
 hl.window_rule({ match = { class = "chrome-youtube.com__-Default" }, workspace = "special:media" })
 
-hl.window_rule({ match = { class = "discord"}, workspace = "special:social"})
-
+hl.window_rule({ match = { class = "discord" }, workspace = "special:social" })
 
 hl.window_rule({ match = { class = "steam_app_2483190" }, opaque = true })
 
@@ -92,74 +89,72 @@ hl.window_rule({ match = { class = "steam_app_2483190" }, immediate = true })
 
 hl.window_rule({ match = { fullscreen = true }, opaque = true })
 hl.window_rule({ match = { float = true }, opaque = true })
-hl.window_rule({ match = { title = "web-panel — Zen Browser"}, opaque = true})
+hl.window_rule({ match = { title = "web-panel — Zen Browser" }, opaque = true })
 
 hl.window_rule({ match = { class = "cs2" }, immediate = true })
 
-hl.window_rule({ match = { title = "btop"}, float= true})
+hl.window_rule({ match = { title = "btop" }, float = true })
 
 ------
 --- Dashboard Workspace 3
 ------
 
 hl.window_rule({
-    match       = { class = "dash-nvtop" },
-    workspace   = "3",
-    float     = true,
-    move        = { "22", "62" },
-    size        = { "375", "505" }
+	match = { class = "dash-nvtop" },
+	workspace = "3",
+	float = true,
+	move = { "22", "62" },
+	size = { "375", "505" },
 })
 
 hl.window_rule({
-    match       = { class = "dash-btop" },
-    workspace   = "3",
-    float     = true,
-    move        = { "411", "62" },
-    size        = { "1487", "505" }
+	match = { class = "dash-btop" },
+	workspace = "3",
+	float = true,
+	move = { "411", "62" },
+	size = { "1487", "505" },
 })
 
 hl.window_rule({
-    match       = { class = "dash-cava" },
-    workspace   = "3",
-    float     = true,
-    move        = { "22", "824" },
-    size        = { "378", "234" }
+	match = { class = "dash-cava" },
+	workspace = "3",
+	float = true,
+	move = { "22", "824" },
+	size = { "378", "234" },
 })
 
 hl.window_rule({
-    match       = { class = "dash-pipes" },
-    workspace   = "3",
-    float     = true,
-    move        = { "22", "580" },
-    size        = { "378", "234" }
+	match = { class = "dash-pipes" },
+	workspace = "3",
+	float = true,
+	move = { "22", "580" },
+	size = { "378", "234" },
 })
 
 hl.window_rule({
-    match       = { class = "dash-lazy" },
-    workspace   = "3",
-    float     = true,
-    move        = { "414", "581" },
-    size        = { "1484", "477" }
+	match = { class = "dash-lazy" },
+	workspace = "3",
+	float = true,
+	move = { "414", "581" },
+	size = { "1484", "477" },
 })
-
 
 ------
 --- Layer Rules
 ------
 
 hl.layer_rule({
-    match        = { namespace = "swaync-control-center" },
-    blur         = true,
-    ignore_alpha = 0.5
+	match = { namespace = "swaync-control-center" },
+	blur = true,
+	ignore_alpha = 0.5,
 })
 
 -- Reglas para las ventanas emergentes de notificación de SwayNC
 hl.layer_rule({
-    match        = { namespace = "swaync-notification-window" },
-    blur         = true,
-    ignore_alpha = 0.5
+	match = { namespace = "swaync-notification-window" },
+	blur = true,
+	ignore_alpha = 0.5,
 })
-
 
 ------
 --- Workspace Rules
