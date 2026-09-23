5b57846d924765cf5db8997744805ffa122a8cff
luisnisc <luisnisc30mayo2005@gmail.com>
2026-09-23 11:34:52 +0200

    .

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -11,6 +11,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 	hl.exec_cmd("hypridle")
+	hl.exec_cmd("hyprsunset")
 	hl.exec_cmd("spicetify watch -s &")
 end)
 
